eddb8b1f68d711d8d9e2e3c5e77f049c81a91ed2
Breno Teixeira <inf.teixeira@gmail.com>
2018-10-31 17:05:07 -0300

    Troca da tabela dos usuários e adição dos icones da tabela

--- locadora/lista_usuarios.php
+++ locadora/lista_usuarios.php
@@ -13,61 +13,56 @@
   </div>
 </div>
 
-<div class="container">
-     
-    <div class="row">
-        <div class="col-xs-12">
-          <div class="box">
+<div class="container">  
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">Usuários</h3>
+        </div>
 
-            <div class="box-header">
-              <h3 class="box-title">USUÁRIOS</h3>
-
-              <div class="box-tools">
-              <ul class="pagination pagination-sm no-margin pull-right">
-                  <li><a href="#">&laquo;</a></li>
-                  <li><a href="#">1</a></li>
-                  <li><a href="#">2</a></li>
-                  <li><a href="#">3</a></li>
-                  <li><a href="#">&raquo;</a></li>
-                </ul>
-              </div>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body table-responsive no-padding">
-              <table class="table table-hover">
-                <tr>
-                  <th>ID</th>
-                  <th>Nome</th>
-                  <th>CPF</th>
-                  <th>E-mail</th>
-                  <th>STATUS</th>
-                  <th></th>
-                </tr>
-                <?php
-                  foreach ($resultado as $users) {
-                    if($users['status'] == 1){
-                      $situacao = '<span class="label label-success">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ativo&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>';
-                    } else {
-                      $situacao = '<span class="label label-danger">Desativado</span>';
-                    }
-                  ?>
-                  <tr>
-                      <td><?php echo $users['idUsuario'] ?></td>
-                      <td><?php echo $users['nome'] ?></td>
-                      <td><?php echo $users['cpf'] ?></td>
-                      <td><?php echo $users['email'] ?></td>
-                      <td><?php echo $situacao?></td>
-                  </tr>
-                  <?php
-                  };
-                ?>
-              </table>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
+        <div class="box-body">
+          <table id="usuarios" class="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>CPF</th>
+                <th>E-mail</th>
+                <th>STATUS</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody style="text-align: center;">
+              <?php
+              foreach ($resultado as $users) {
+              if($users['status'] == 1){
+                $situacao = '<span class="label label-success">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ativo&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>';
+              } else {
+                $situacao = '<span class="label label-danger">Desativado</span>';
+              }
+              ?>
+              <tr>
+              <td><?php echo $users['idUsuario'] ?></td>
+              <td><?php echo $users['nome'] ?></td>
+              <td><?php echo $users['cpf'] ?></td>
+              <td><?php echo $users['email'] ?></td>
+              <td><?php echo $situacao?></td>
+              <td style="text-align: center; width: 100px;">
+              <a class="glyphicon glyphicon-search"></a>&nbsp;&nbsp;&nbsp;
+              <a class="glyphicon glyphicon-edit"></a>&nbsp;&nbsp;&nbsp;
+              <a class="glyphicon glyphicon-remove"></a>
+              </td>
+            </tr>
+            <?php
+            };
+            ?>
+            </tbody>
+          </table>
         </div>
       </div>
+    </div>
+  </div>
 </div>
 
 
