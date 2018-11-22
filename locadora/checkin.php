eaa8221a7629f6a44f69fe15278440c68c828b2c
Breno <inf.teixeira@gmail.com>
2018-11-22 00:46:40 -0300

    Atualização da tabela Check-in

--- locadora/checkin.php
+++ locadora/checkin.php
@@ -1,38 +1,76 @@
 <?php
-include 'model/checkin_pendente.php';
+  include 'model/header.php';
+  include 'model/checkin_pendente.php';
 
 ?>
-<table border="1">
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="singlebutton"></label>
+  <div class="col-md-4">
+    
+  </div>
+</div>
+
+<div class="container">  
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">Reservas para Check-In</h3>
+        </div>
+          <div class="tab-content">
+
+              <div class="box-body table-responsive no-padding">
+                <table id="formapagamentos" class="table table-bordered table-striped">
 <thead>
                     <tr>
                       <th><center>Id Reserva</center></th>
                       <th><center>Carro Selecionado</center></th>
                       <th><center>Usuario</center></th>
-                      <th><center>data_inicio</center></th>
-                      <th><center>data_fim</center></th>
+                      <th><center>Data de Inicio</center></th>
+                      <th><center>Data Final</center></th>
                       <th><center>Forma Pagamento</center></th>
                       <th><center>Data da Reserva</center></th>
                       <th><center>valor da Reserva</center></th>
-                      <th><center><a href="cadastra_veiculo.php" class="glyphicon glyphicon-plus"> NOVO</a>
+                      <th><center>Check-In</a>
                     </tr>
                   </thead>
                   <tbody style="text-align: center;">
                     <?php
                     foreach ($resultado as $dado_reserva) {
+                      $dt_inicio  = str_replace("-", "/", $dado_reserva['data_inicio']);
+                      $dt_fim     = str_replace("-", "/", $dado_reserva['data_fim']);
+                      $dt_reserva = str_replace("-", "/", $dado_reserva['data_reserva']);
+                      $datac1 = date('d/m/Y', strtotime($dt_inicio));
+                      $datac2 = date('d/m/Y', strtotime($dt_fim));
+                      $datac3 = date('d/m/Y H:i', strtotime($dt_reserva));
                     ?>
                     <tr>
-                    <td><?php echo $dado_reserva['idReserva'] ?></td>
+                    <td><?php echo "#".$dado_reserva['idReserva'] ?></td>
                     <td><?php echo $dado_reserva['modelo'] ?></td>
                     <td><?php echo $dado_reserva['nome'] ?></td>
-                    <td><?php echo $dado_reserva['data_inicio'] ?></td>
-                    <td><?php echo $dado_reserva['data_fim'] ?></td>
+                    <td><?php echo $datac1 ?></td>
+                    <td><?php echo $datac2 ?></td>
                     <td><?php echo $dado_reserva['descricao'] ?></td>
-                    <td><?php echo $dado_reserva['data_reserva'] ?></td>
-                    <td><?php echo $dado_reserva['valorReserva'] ?></td>
-                    <td><a href="model/inserir_checkin.php?id=<?php echo $dado_reserva['idReserva'] ?>">Checkin</a></td>
+                    <td><?php echo $datac3 ?></td>
+                    <td><?php echo "R$ ".$dado_reserva['valorReserva'] ?></td>
+                    <td><a href="model/inserir_checkin.php?id=<?php echo $dado_reserva['idReserva'] ?>"onclick='return confirm("Confirmar Check-In dessa reserva?");' class="glyphicon glyphicon-check"></a></td>
+                    
                   </tr>
                   <?php
                   };
                   ?>
                   </tbody>
                 </table>
+              </div>
+            
+          </div>
+        </div>
+    </div>
+  </div>
+</div>
+  
+<?php
+  include 'model/footer.php'
+?>
+
